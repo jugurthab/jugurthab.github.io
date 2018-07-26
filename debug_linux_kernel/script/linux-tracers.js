14cec8f12792f80907d788cee77aac7bb9c81528
Jugurtha BELKALEM <jugurthabelkalem@gmail.com>
2018-07-26 21:56:51 +0200

    Adding resizing response to animation

--- debug_linux_kernel/script/linux-tracers.js
+++ debug_linux_kernel/script/linux-tracers.js
@@ -24,6 +24,8 @@ var eventContainerArray = new Array();
 
 var allowedKernelEvents = new Array('I','M', 'D','S');
 var CounterallowedKernelEvents = new Array(0, 0, 0, 0);
+var allowedKernelEventsColor = new Array('#270101','#720017', '#D8D583','#D9AC2A', '#763F02');
+
 
 var main_container, linux_container, linux_container_userspace, linux_container_kernel, main_tracer_profiler, main_tracer_profiler_probe;
 
@@ -107,7 +109,7 @@ $(document).ready(function(e){
 
 		function drawTracerOrProfiler(){
 			
-				main_tracer_profiler = new mainContainer(linux_container_kernel.x+linux_container_kernel.width, canvasHeightTracers/3, linux_container_userspace.x - (linux_container_kernel.x+linux_container_kernel.width), canvasHeightTracers/4,"rgba(0,128,0,0.8)", "Tracer","rgba(255,255,255,1.0)");
+				main_tracer_profiler = new mainContainer(linux_container_kernel.x+linux_container_kernel.width, linux_container_kernel.y + linux_container_kernel.height/2-canvasHeightTracers/8, linux_container_userspace.x - (linux_container_kernel.x+linux_container_kernel.width), canvasHeightTracers/4,"#04060F", "Tracer","rgba(255,255,255,1.0)");
 			if($("#selectTool").val()=="profiler")		
 				main_tracer_profiler.text = "Profiler";
 
@@ -118,17 +120,19 @@ console.log($("#selectTool").val());
 			
 			contextTracers.fillStyle = main_tracer_profiler.colorText;
 			contextTracers.font = "30px serif";
-			contextTracers.fillText(main_tracer_profiler.text, main_tracer_profiler.x +8, main_tracer_profiler.y + main_tracer_profiler.height/2+8);
+            			    contextTracers.save();
+			    contextTracers.rotate(-Math.PI/2);
+			contextTracers.fillText(main_tracer_profiler.text, -main_tracer_profiler.y - main_tracer_profiler.height/3, main_tracer_profiler.x +8);
+            contextTracers.restore();
 
-
-			main_tracer_profiler_probe = new mainContainer(main_tracer_profiler.x-50, main_tracer_profiler.y+40, 50, 50,"rgba(255,0,0,0.8)", "Probe","rgba(255,255,255,1.0)");
+			main_tracer_profiler_probe = new mainContainer(main_tracer_profiler.x-50, linux_container_kernel.y + linux_container_kernel.height/2-canvasHeightTracers/16, 50, canvasHeightTracers/8,"#03353E", "Probe","rgba(255,255,255,1.0)");
 			contextTracers.fillStyle = main_tracer_profiler_probe.color;
-			contextTracers.fillRect(main_tracer_profiler_probe.x, main_tracer_profiler_probe.y, main_tracer_profiler_probe.width, main_tracer_profiler_probe.height);
+			contextTracers.fillRect(main_tracer_profiler_probe.x,main_tracer_profiler_probe.y , main_tracer_profiler_probe.width, main_tracer_profiler_probe.height);
 
 			
 			contextTracers.fillStyle = main_tracer_profiler_probe.colorText;
 			contextTracers.font = "15px serif";
-			contextTracers.fillText(main_tracer_profiler_probe.text, main_tracer_profiler_probe.x+3 , main_tracer_profiler_probe.y+30);
+			contextTracers.fillText(main_tracer_profiler_probe.text, main_tracer_profiler_probe.x+8 , main_tracer_profiler_probe.y +main_tracer_profiler_probe.height/2+5);
 
 		}
 
@@ -139,12 +143,13 @@ console.log($("#selectTool").val());
 				x = Math.floor(20+linux_container_kernel.x + Math.random() * (linux_container_kernel.width - 90));
 				y = Math.floor(50+linux_container_kernel.y + Math.random() * (linux_container_kernel.height - 70));
 				radius = 20;
-				var r = Math.floor(Math.random() * 255);
-				var g = Math.floor(Math.random() * 255);
-				var b = Math.floor(Math.random() * 255);
-				color = 'rgb('+r+','+g+','+b+')';
+
 				text = allowedKernelEvents[Math.floor(Math.random()*allowedKernelEvents.length)];
-				colorText = 'rgb('+g+','+b+','+r+')';
+
+                var index_Event = allowedKernelEvents.indexOf(text);
+                console.log(index_Event)
+                color = allowedKernelEventsColor[index_Event];
+				colorText = 'rgb(255,255,255)';
 				eventContainerArray.push(new eventContainer(x,y,radius,color,text,colorText));
 				contextTracers.fillStyle = eventContainerArray[i].color;
 				contextTracers.beginPath();
@@ -161,20 +166,20 @@ console.log($("#selectTool").val());
 		
 
 		function drawUserSpace(){
-			linux_container_userspace = new mainContainer(canvasWidthTracers/2,120,canvasWidthTracers/3,canvasHeightTracers/2,"rgba(0,128,255,0.8)", "Userspace","rgba(255,255,255,1.0)");
+			linux_container_userspace = new mainContainer(canvasWidthTracers/2,linux_container.y+40,canvasWidthTracers/3,linux_container.height-60,"#D9AC2A", "Userspace","rgba(255,255,255,1.0)");
 
-			contextTracers.fillStyle = linux_container.color;
+			contextTracers.fillStyle = linux_container_userspace.color;
 			contextTracers.fillRect(linux_container_userspace.x, linux_container_userspace.y, linux_container_userspace.width, linux_container_userspace.height);
 
 			
 			contextTracers.fillStyle = linux_container_userspace.colorText;
 			contextTracers.font = "30px serif";
-			contextTracers.fillText(linux_container_userspace.text, linux_container_userspace.x + linux_container_userspace.width/2 - 80, linux_container_userspace.y+35);
+			contextTracers.fillText(linux_container_userspace.text, linux_container_userspace.x + linux_container_userspace.width/2 - 60, linux_container_userspace.y+35);
 		}
 
 
 		function drawKernelSpace(){
-			linux_container_kernel = new mainContainer(canvasWidthTracers/10,120,canvasWidthTracers/3,canvasHeightTracers/2,"rgba(0,0,255,0.8)", "Kernel","rgba(255,255,255,1.0)");
+			linux_container_kernel = new mainContainer(canvasWidthTracers/10,linux_container.y+40,canvasWidthTracers/3,linux_container.height-60,"#A79C93", "Kernel","rgba(255,255,255,1.0)");
 
 			contextTracers.fillStyle = linux_container_kernel.color;
 			contextTracers.fillRect(linux_container_kernel.x, linux_container_kernel.y, linux_container_kernel.width, linux_container_kernel.height);
@@ -187,7 +192,7 @@ console.log($("#selectTool").val());
 
 
 		function drawLinux(){
-			linux_container = new mainContainer(canvasWidthTracers/40,60,canvasWidthTracers - canvasWidthTracers/20,canvasHeightTracers/2+ canvasHeightTracers/3 -60,"rgba(255,128,255,0.5)", "Linux OS","rgba(255,255,255,1.0)");
+			linux_container = new mainContainer(canvasWidthTracers/40,60,canvasWidthTracers - canvasWidthTracers/20,canvasHeightTracers/2+ canvasHeightTracers/3 -60,"#C1403D", "Linux OS","rgba(255,255,255,1.0)");
 
 			contextTracers.fillStyle = linux_container.color;
 			contextTracers.fillRect(linux_container.x, linux_container.y, linux_container.width, linux_container.height);
@@ -199,7 +204,7 @@ console.log($("#selectTool").val());
 		}
 
 		function drawContainerMachine(){
-			main_container = new mainContainer(canvasWidthTracers/50,20,canvasWidthTracers - canvasWidthTracers/30,canvasHeightTracers/2+ canvasHeightTracers/3 +20,"rgba(255,128,0,0.5)", "Host Machine","rgba(255,255,255,1.0)");
+			main_container = new mainContainer(canvasWidthTracers/50,20,canvasWidthTracers - canvasWidthTracers/30,canvasHeightTracers/2+ canvasHeightTracers/3 +20,"#0294A5", "Host Machine","rgba(255,255,255,1.0)");
 
 			contextTracers.fillStyle = main_container.color;
 			contextTracers.fillRect(main_container.x, main_container.y, main_container.width, main_container.height);
@@ -218,9 +223,11 @@ console.log($("#selectTool").val());
 			drawContainerMachine();
 			drawLinux();
 
-			drawUserSpace();
+
 	
 			drawKernelSpace();
+
+			drawUserSpace();
 			eventContainerArray= new Array();
 
 			
@@ -243,6 +250,7 @@ console.log($("#selectTool").val());
                     if(timerJavascript)
                         clearTimeout(timerJavascript);
                     canvasTracers.attr("width",$(window).get(0).innerWidth - 100);
+                    canvasTracers.attr("height",$(window).get(0).innerHeight - 250);
                     canvasWidthTracers = canvasTracers.width();
                     canvasHeightTracers = canvasTracers.height();
                     
