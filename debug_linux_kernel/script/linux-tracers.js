7aa38f0f066630a3e7e68058dbf28d1eea113de5
Jugurtha BELKALEM <jugurtha.belkalem@smile.fr>
2018-07-27 10:02:23 +0200

    Adding userspace graph report

--- debug_linux_kernel/script/linux-tracers.js
+++ debug_linux_kernel/script/linux-tracers.js
@@ -23,16 +23,18 @@ var eventContainer = function(x, y, radius, color, text, colortext){
 var eventContainerArray = new Array();
 
 var allowedKernelEvents = new Array('I','M', 'D','S');
+var allowedKernelEventsMeaning = new Array('Interrupt', 'Memory access', 'Disk access', 'Scheduler event');
 var CounterallowedKernelEvents = new Array(0, 0, 0, 0);
 var allowedKernelEventsColor = new Array('#270101','#720017', '#D8D583','#D9AC2A', '#763F02');
 
+var counterTimestamp = new Array(0,0,0,0);
 
 var main_container, linux_container, linux_container_userspace, linux_container_kernel, main_tracer_profiler, main_tracer_profiler_probe;
 
 var timerJavascript = false;
 var animateScene =false;
 
-var NUMBER_OF_EVENTS = 5;
+var NUMBER_OF_EVENTS = 4;
 
 $(document).ready(function(e){
                 var canvasTracers = $("#linuxTracersAnim");
@@ -62,6 +64,36 @@ $(document).ready(function(e){
 
 		$("#selectTool").on("change",resizeCanvasTracer);
 		
+
+
+
+		function drawKeyMap(){
+			//'I','M', 'D','S'
+			for(var i=0;i<NUMBER_OF_EVENTS;i++){
+				contextTracers.fillStyle=allowedKernelEventsColor[i];
+				contextTracers.beginPath();
+				contextTracers.arc(60+ 170 * i,40, eventContainerArray[i].radius, 0, Math.PI*2, false);
+				contextTracers.closePath();
+				contextTracers.fill();
+
+				contextTracers.fillStyle="rgb(255,255,255)";
+				contextTracers.font = "20px serif";
+				contextTracers.fillText(allowedKernelEvents[i], 60+ 170 * i-8, 47);
+
+
+
+				contextTracers.fillStyle="rgb(255,255,255)";
+				contextTracers.font = "15px serif";
+				contextTracers.fillText(allowedKernelEventsMeaning[i], 80+ 170 * i, 47);
+
+			}
+
+
+
+				
+			
+		}
+
 		function drawLinesEventToTracer(){
 			for(var i=0;i<NUMBER_OF_EVENTS;i++){			
 				// Draw lines to tracer
@@ -77,12 +109,12 @@ $(document).ready(function(e){
 
 		function drawEvents(){
 			CounterallowedKernelEvents =new Array(0, 0, 0, 0);
-			text = "<ul>";
+			text = "<table>";
 			counter = 0;
 			for(var i=0;i<NUMBER_OF_EVENTS;i++){
-		                if($("#selectTool").val()=="tracer")			{	
-					counter += Math.floor(Math.random() * 20);
-					text += "<li>Timestamp : " + counter + " ==> Event " + eventContainerArray[i].text + "</li>";
+		                if($("#selectTool").val()=="tracer")			{					counter+=Math.floor(Math.random()*5);
+					counterTimestamp[i] = counter;
+					text += "<tr><td>Timestamp : " + counterTimestamp[i] + " </td><td> Event " + eventContainerArray[i].text + "</td></tr>";
 				}
 
 				else if($("#selectTool").val()=="profiler"){
@@ -95,16 +127,86 @@ $(document).ready(function(e){
 
 			if($("#selectTool").val()=="profiler"){
 				for(var i = 0; i < CounterallowedKernelEvents.length; i++){
-					text += "<li>Event : " + allowedKernelEvents[i] + " produced : " + CounterallowedKernelEvents[i] + " times</li>";
+					text += "<tr><td>Event : " + allowedKernelEvents[i] + " </td><td> " + CounterallowedKernelEvents[i] + " times</td></tr>";
 				}
 			}
 
-			text+="</ul>";
+			text+="</table>";
 			$("#logData").empty();
 			$("#logData").append(text);
 
-
+			//counterTimestamp.sort(function(a, b){return a-b});
 			
+		}
+
+
+		function drawGraph(){
+
+			if($("#selectTool").val()=="profiler"){			
+				for(var i=0;i<NUMBER_OF_EVENTS;i++){
+					contextTracers.beginPath(); 
+					contextTracers.moveTo(60+ linux_container_userspace.x + linux_container_userspace.width/5 * i, linux_container_userspace.y + linux_container_userspace.height -40);
+
+					contextTracers.lineTo(60+linux_container_userspace.x + linux_container_userspace.width/5 * i, linux_container_userspace.y + linux_container_userspace.height -40 - CounterallowedKernelEvents[i] * 70);
+
+
+					contextTracers.closePath(); 
+					contextTracers.stroke();
+				
+
+					contextTracers.fillStyle = "rgb(255,0,0)";
+					contextTracers.font = "20px serif";
+					contextTracers.fillText(allowedKernelEvents[i], linux_container_userspace.x + linux_container_userspace.width/5 * i +55, linux_container_userspace.y + linux_container_userspace.height -10);
+
+
+
+
+
+
+
+
+					// draw vertical scale axis
+					contextTracers.fillStyle = eventContainerArray[i].colorText;
+					contextTracers.font = "20px serif";
+					contextTracers.fillText(i, linux_container_userspace.x +20, linux_container_userspace.y + linux_container_userspace.height -40 - i * 70);
+	
+
+				}	
+
+			}
+
+		
+			else if($("#selectTool").val()=="tracer"){
+				console.log("hi");
+
+				for(var i=0;i<NUMBER_OF_EVENTS;i++){
+					
+					contextTracers.beginPath(); 
+					contextTracers.moveTo( linux_container_userspace.x + linux_container_userspace.width/5 * i +55, linux_container_userspace.y + linux_container_userspace.height -40);
+
+					contextTracers.lineTo(linux_container_userspace.x + linux_container_userspace.width/5 * i +55, linux_container_userspace.y + linux_container_userspace.height/2 -10);
+
+
+					contextTracers.closePath(); 
+					contextTracers.stroke();
+
+
+					contextTracers.fillStyle = "rgb(255,0,0)";
+					contextTracers.font = "20px serif";
+					contextTracers.fillText(eventContainerArray[i].text, linux_container_userspace.x + linux_container_userspace.width/5 * i +55, linux_container_userspace.y + linux_container_userspace.height/2 -10);
+
+					
+					contextTracers.fillStyle = "rgb(255,255,255)";
+					contextTracers.font = "20px serif";
+					contextTracers.fillText(counterTimestamp[i], linux_container_userspace.x + linux_container_userspace.width/5 * i +55, linux_container_userspace.y + linux_container_userspace.height -10);
+
+
+
+
+				}				
+   			}
+
+	
 		}
 
 		function drawTracerOrProfiler(){
@@ -113,16 +215,15 @@ $(document).ready(function(e){
 			if($("#selectTool").val()=="profiler")		
 				main_tracer_profiler.text = "Profiler";
 
-console.log($("#selectTool").val());
 			contextTracers.fillStyle = main_tracer_profiler.color;
 			contextTracers.fillRect(main_tracer_profiler.x, main_tracer_profiler.y, main_tracer_profiler.width, main_tracer_profiler.height);
 
 			
 			contextTracers.fillStyle = main_tracer_profiler.colorText;
 			contextTracers.font = "30px serif";
-            			    contextTracers.save();
-			    contextTracers.rotate(-Math.PI/2);
-			contextTracers.fillText(main_tracer_profiler.text, -main_tracer_profiler.y - main_tracer_profiler.height/3, main_tracer_profiler.x +8);
+            		contextTracers.save();
+			contextTracers.rotate(-Math.PI/2);
+			contextTracers.fillText(main_tracer_profiler.text, -main_tracer_profiler.y - main_tracer_profiler.height/2 -50, main_tracer_profiler.x + main_tracer_profiler.width/2+8);
             contextTracers.restore();
 
 			main_tracer_profiler_probe = new mainContainer(main_tracer_profiler.x-50, linux_container_kernel.y + linux_container_kernel.height/2-canvasHeightTracers/16, 50, canvasHeightTracers/8,"#03353E", "Probe","rgba(255,255,255,1.0)");
@@ -132,7 +233,11 @@ console.log($("#selectTool").val());
 			
 			contextTracers.fillStyle = main_tracer_profiler_probe.colorText;
 			contextTracers.font = "15px serif";
-			contextTracers.fillText(main_tracer_profiler_probe.text, main_tracer_profiler_probe.x+8 , main_tracer_profiler_probe.y +main_tracer_profiler_probe.height/2+5);
+
+			contextTracers.save();
+			contextTracers.rotate(-Math.PI/2);
+			contextTracers.fillText(main_tracer_profiler_probe.text , -main_tracer_profiler_probe.y -main_tracer_profiler_probe.height/2 -20,main_tracer_profiler_probe.x+main_tracer_profiler_probe.width/2+1);
+			contextTracers.restore();
 
 		}
 
@@ -147,7 +252,7 @@ console.log($("#selectTool").val());
 				text = allowedKernelEvents[Math.floor(Math.random()*allowedKernelEvents.length)];
 
                 var index_Event = allowedKernelEvents.indexOf(text);
-                console.log(index_Event)
+
                 color = allowedKernelEventsColor[index_Event];
 				colorText = 'rgb(255,255,255)';
 				eventContainerArray.push(new eventContainer(x,y,radius,color,text,colorText));
@@ -174,7 +279,7 @@ console.log($("#selectTool").val());
 			
 			contextTracers.fillStyle = linux_container_userspace.colorText;
 			contextTracers.font = "30px serif";
-			contextTracers.fillText(linux_container_userspace.text, linux_container_userspace.x + linux_container_userspace.width/2 - 60, linux_container_userspace.y+35);
+			contextTracers.fillText(linux_container_userspace.text, linux_container_userspace.x + linux_container_userspace.width/2 - 80, linux_container_userspace.y+35);
 		}
 
 
@@ -187,7 +292,7 @@ console.log($("#selectTool").val());
 			
 			contextTracers.fillStyle = linux_container_kernel.colorText;
 			contextTracers.font = "30px serif";
-			contextTracers.fillText(linux_container_kernel.text, linux_container_kernel.x + linux_container_kernel.width/2 - 60, linux_container_kernel.y+35);
+			contextTracers.fillText(linux_container_kernel.text, linux_container_kernel.x + linux_container_kernel.width/2 - 50, linux_container_kernel.y+35);
 		}
 
 
@@ -240,7 +345,9 @@ console.log($("#selectTool").val());
 			drawLinesEventToTracer();
 
 			drawEvents();
+			drawGraph();
 
+			drawKeyMap();
 			if(animateScene){
              			timerJavascript = setTimeout(drawTracersProfilersScene, 1000);
 			}
